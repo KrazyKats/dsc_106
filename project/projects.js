fcda9f95dcc3d40c498fd1cbb8f6d4b9467bf275
KrazyKatz <qndo@ucsd.edu>
2025-04-25 15:14:06 -0700

    path

--- project/projects.js
+++ project/projects.js
@@ -3,7 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projectsContainer = document.querySelector('.projects');
 const projectsTitle = document.querySelector('.projects-title');
 
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('lib/projects.json');
 renderProjects(projects, projectsContainer, 'h2');
 
 // Step 1.6 - Count Projects
