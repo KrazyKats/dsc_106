ce2b9991fb1360416e653633d552ad52ec1e8fff
KrazyKatz <qndo@ucsd.edu>
2025-05-01 21:11:03 -0700

    fix double

--- project/projects.js
+++ project/projects.js
@@ -13,41 +13,6 @@ if (projectsTitle && Array.isArray(projects)) {
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-d3.json('../lib/projects.json').then((projects) => {
-  // 1. Group and convert data
-  let rolledData = d3.rollups(projects, v => v.length, d => d.year);
-  let data = rolledData.map(([year, count]) => ({
-    value: count,
-    label: year
-  }));
-
-  // 2. Slice + arc generation
-  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-  let sliceGenerator = d3.pie().value(d => d.value);
-  let arcData = sliceGenerator(data);
-  let arcs = arcData.map(d => arcGenerator(d));
-
-  // 3. Color palette
-  let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-  // 4. Draw slices
-  arcs.forEach((arc, idx) => {
-    d3.select('#projects-pie-plot')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors(idx));
-  });
-
-  // 5. Create legend
-  let legend = d3.select('.legend');
-  data.forEach((d, idx) => {
-    legend.append('li')
-      .attr('class', 'legend-item')
-      .attr('style', `--color:${colors(idx)}`)
-      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-  });
-});
-
 
 let query = '';
 let searchInput = document.querySelector('.searchBar');
